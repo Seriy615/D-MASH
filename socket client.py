79e5d6a4658c7ae2eb3108bf6c770e499c5bc8c7
Seriy615 <140434578+Seriy615@users.noreply.github.com>
2024-05-17 13:44:07 +0300

    Update socket client.py

--- socket client.py
+++ socket client.py
@@ -1,13 +1,22 @@
 import socket
 
-HOST, PORT = "localhost", 12345 
+def send_data_to_server(data, server_ip, server_port):
+    try:
+        # Создаем сокет
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Подключаемся к серверу
+        client_socket.connect((server_ip, server_port))
+        # Отправляем данные
+        client_socket.sendall(data.encode('utf-8'))
+        # Закрываем соединение
+        client_socket.close()
+    except Exception as e:
+        print(f"Ошибка при отправке данных: {e}")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
+if __name__ == "__main__":
+    server_ip = "10.10.2.147"  # IP-адрес сервера
+    server_port = 12345      # Порт сервера
+
     while True:
-        data = input("Type the message to send:")
-        data_bytes = data.encode()  # str to bytes
-        sock.sendall(data_bytes)
-        data_bytes = sock.recv(1024)
-        data = data_bytes.decode()  # bytes to str
-        print("Received:", data)
+        data = input("Введите данные для отправки на сервер: ")
+        send_data_to_server(data, server_ip, server_port)
