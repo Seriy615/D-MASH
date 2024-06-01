c762c78a0563de7f2592271991e4e015749b59fd
Seriy615 <140434578+Seriy615@users.noreply.github.com>
2024-06-01 11:27:52 +0300

    Delete D-MASH directory

--- D-MASH/getip.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def get_local_ip():
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-    return ip_address
-
-if __name__ == "__main__":
-    print("Local IP address:", get_local_ip())
